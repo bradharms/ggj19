eda32ff1c1538f68533e31d9ce7f00172e846fc2
Bradley K. Harms <brad.k.harms@gmail.com>
2019-01-25 21:03:05 -0600

    checkpoint

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,24 @@
-import {AmbientLight} from 'three';
+import {
+    PerspectiveCamera,
+    Scene,
+    BoxBufferGeometry,
+    MeshPhongMaterial
+} from 'three';
 
 class Main {
+    camera = new PerspectiveCamera(
+        70,
+        window.innerWidth / window.innerHeight,
+        1,
+        1000
+    )
+
+    scene = new Scene()
+
+    geometry = new BoxBufferGeometry(200, 200, 200)
+
     constructor() {
-        const a = new AmbientLight();
+        this.camera.position.z = 400;
     }
 }
 
